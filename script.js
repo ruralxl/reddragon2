14414d61bab7b8f25862fe8f94b1418f7a15b825
G0dUssop <gus200809@gmail.com>
2024-04-21 15:14:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-const arrowBtns = document.querySelectorAll(".wrapper i");
-const carousel = document.querySelector(".caroussel");
-const firstCardWidth = carousel.querySelector(".cartas").offsetWidth;
 
-arrowBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+const seta_esquerda = document.getElementById("left");
+const seta_direita = document.getElementById("right");
+
+const seta_esquerda2 = document.getElementById("esquerda");
+const seta_direita2 = document.getElementById("direita");
+
+const carousel = document.querySelector(".caroussel");
+const tamanho1= carousel.querySelector(".cartas").offsetWidth;
+
+const carousel2 = document.querySelector(".caroussel2");
+const tamanho2 = carousel2.querySelector(".cartas").offsetWidth;
+
+    seta_esquerda.addEventListener("click", () => {
+        carousel.scrollLeft += -tamanho1; 
     });
-});
+
+    seta_direita.addEventListener("click", () => {
+        carousel.scrollLeft += tamanho1;
+    });
+
+    seta_esquerda2.addEventListener("click", () => {
+        carousel2.scrollLeft += -tamanho2; 
+    });
+
+    seta_direita2.addEventListener("click", () => {
+        carousel2.scrollLeft += tamanho2;
+    });
+
+
 
 
