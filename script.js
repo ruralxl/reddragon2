ab39cf96c8302add766a7d68458c380a430b8edb
G0dUssop <gus200809@gmail.com>
2024-04-22 12:15:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,27 +5,56 @@ const seta_direita = document.getElementById("right");
 const seta_esquerda2 = document.getElementById("esquerda");
 const seta_direita2 = document.getElementById("direita");
 
+const seta_esquerda3 = document.getElementById("esquerda3");
+const seta_direita3 = document.getElementById("direita3");
+
+const seta_esquerda4 = document.getElementById("esquerda4");
+const seta_direita4 = document.getElementById("direita4");
+
 const carousel = document.querySelector(".caroussel");
-const tamanho1= carousel.querySelector(".cartas").offsetWidth;
 
 const carousel2 = document.querySelector(".caroussel2");
-const tamanho2 = carousel2.querySelector(".cartas").offsetWidth;
+
+const carousel3 = document.querySelector(".caroussel3");
+
+const carousel4 = document.querySelector(".caroussel4");
+
+const tamanho= carousel.querySelector(".cartas").offsetWidth;
+
 
     seta_esquerda.addEventListener("click", () => {
-        carousel.scrollLeft += -tamanho1; 
+        carousel.scrollLeft += -tamanho; 
     });
 
     seta_direita.addEventListener("click", () => {
-        carousel.scrollLeft += tamanho1;
+        carousel.scrollLeft += tamanho;
     });
 
     seta_esquerda2.addEventListener("click", () => {
-        carousel2.scrollLeft += -tamanho2; 
+        carousel2.scrollLeft += -tamanho; 
     });
 
     seta_direita2.addEventListener("click", () => {
-        carousel2.scrollLeft += tamanho2;
+        carousel2.scrollLeft += tamanho;
     });
+
+    seta_esquerda3.addEventListener("click", () => {
+        carousel3.scrollLeft += -tamanho; 
+    });
+
+    seta_direita3.addEventListener("click", () => {
+        carousel3.scrollLeft += tamanho;
+    });
+
+
+    seta_esquerda4.addEventListener("click", () => {
+        carousel4.scrollLeft += -tamanho; 
+    });
+
+    seta_direita4.addEventListener("click", () => {
+        carousel4.scrollLeft += tamanho;
+    });
+
 
 
 
